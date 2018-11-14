28414e0948a94259b559f242a837c81e5c5a852c
Avardeth <sonofsparda88@gmail.com>
2018-11-14 21:24:16 +0100

    'postgres'

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : 'root',
+    database : 'smart-brain'
+  }
+});
+
+console.log(postgres.select('*').from('users'))
 
 const app = express();
 
